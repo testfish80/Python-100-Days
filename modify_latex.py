3d70b704a2d28cb2bd3f6892131816af49c6a932
testfish80 <117278414+testfish80@users.noreply.github.com>
2025-10-31 19:23:06 +0800

    Update modify_latex.py

--- modify_latex.py
+++ modify_latex.py
@@ -6,18 +6,13 @@ def modify_latex(filename):
 
     modified_lines = []
     documentclass_inserted = False
-    begin_document_inserted = False
 
     for line in lines:
         modified_lines.append(line)
 
-        if not documentclass_inserted and re.match(r'\\documentclass\[.*\]\{article\}', line):
-            modified_lines.append('\\usepackage{url}\\usepackage{hyperref}\n')
+        if not documentclass_inserted and re.match(r'\\documentclass\[.*\]\{.*\}', line):  # 匹配 documentclass 行
+            modified_lines.append('\\usepackage{url}\\usepackage{hyperref}\\usepackage{ragged2e}\\RaggedRight\n')  # 添加 usepackage 命令
             documentclass_inserted = True
-
-        if not begin_document_inserted and re.match(r'\\begin\{document\}', line):
-            modified_lines.append('\\usepackage{ragged2e}\\RaggedRight\n')
-            begin_document_inserted = True
 
     with open(filename, 'w') as f:
         f.writelines(modified_lines)
